9be585f1ba1ad7db63aa04214c7b684f8f80b208
Saikatsamanta83 <samantasaikat713@gmail.com>
2022-12-11 11:16:06 +0530

    first commit

---
+++ Samanta_fun.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Samanta_fun {
+    public static void main(String[] args) {
+        Scanner userinput =new Scanner(System.in);
+        int hello =userinput.nextInt();
+        int x =userinput.nextInt(); 
+        intu_number(hello,x);
+    }
+    static void intu_number(int x,int y){
+        int a=x*y;
+        System.out.println(a);
+
+    }
+    
+
+    
+    
+}
